c4cc4b0714060a2a5876bfcf162b9e5330bb00d1
Ahmed Elkoumey <ahmedelkoumey1997@gmail.com>
2022-11-19 16:53:01 +0200

    display data from server

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 
 // fetching data
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+import styled from "styled-components";
 
 class Category extends Component {
+  
   constructor(props) {
     super(props);
 
@@ -43,26 +45,37 @@ class Category extends Component {
     console.log(this.state.test);
 
     return (
-      <>
+      <Container>
         <div>category </div>
         <div>
-          {this.state.test.map((item,ind) => {
-            return (
-              <div key={ind}>
-               <p> {item.id}</p>
-                <div>{item.products.description}</div>
-                
-                  <img src={item.gallery} alt={item.name} />
-               
-                <div>{item.name}</div>
-                <br />
-              </div>
-            );
+          {this.state.test.map((category,ind) => {
+         return(
+          <div key={ind}>
+            <div>{category.name}</div>
+            {category.name==="all" && category.products.map((product,index)=>{
+              return(
+                <>
+                <h2>{product.name}</h2>
+                <h2>{product.inStock ? "--inStock":"-out of Stock-"}</h2>
+                <img src={product.gallery[0]} alt={product.name} width="100px"/>
+                </>
+              )
+            })}
+          </div>
+         )
+            console.log(category.products)
           })}
         </div>
-      </>
+      </Container>
     );
   }
 }
 
 export default Category;
+
+
+const Container = styled.div`
+width: 85%;
+margin: auto;
+
+`;
